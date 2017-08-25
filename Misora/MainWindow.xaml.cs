93a692952e7dca10df784485db8523f5b61702be
Khamraj Rohit <Rohit.Khamraj@gmail.com>
2017-08-24 21:53:08 -0400

    Implemented Basic Functions

--- Misora/MainWindow.xaml.cs
+++ Misora/MainWindow.xaml.cs
@@ -15,14 +15,40 @@ using System.Windows.Shapes;
 
 namespace Misora
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private bool _isLoggedIn;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+			string token = MisoraCore.LoadToken();
+			if (!string.IsNullOrWhiteSpace(token))
+			{
+				tokenBox.Text = token;
+				LoginButton_Click(null, null);
+			}
+
+		}
+
+		private async void LoginButton_Click(object sender, RoutedEventArgs e)
+		{
+			if (_isLoggedIn)
+			{
+				await MisoraCore.Logout();
+				tokenBox.IsEnabled = true;
+				LoginButton.Content = "Login";
+			} else
+			{
+				if (_isLoggedIn = await MisoraCore.Login(tokenBox.Text))
+				{
+					tokenBox.IsEnabled = false;
+					LoginButton.Content = "Logout";
+				} else
+				{
+					MessageBox.Show("Login Failed", "Error");
+				}
+			}
 		}
 	}
 }
---
+++ Misora/Misora.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using System.IO;
+using System.Windows;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Misora
+{
+	public static class MisoraCore
+	{
+		private static DiscordSocketClient _client = new DiscordSocketClient();
+		private static string _nowPlayingFile = @"C:\Users\Karuta\Documents\Misc\nowPlaying.txt";
+		private static Timer _timer;
+
+		internal static async Task<bool> Login(string token)
+		{
+			try
+			{
+				_client.Ready += MonitorPlayback;
+				await _client.LoginAsync(TokenType.User, token);
+				await _client.StartAsync();
+				SaveToken(token);
+				return true;
+			}catch(Exception)
+			{
+				return false;
+			}
+		}
+
+
+		internal static async Task Logout()
+		{
+			await _client.LogoutAsync();
+			await _client.StopAsync();
+		}
+
+		internal static void SaveToken(string token)
+			=> File.WriteAllText("token", token);
+
+		internal static string LoadToken()
+		{
+			try
+			{
+				return File.ReadAllText("token");
+			}catch(IOException)
+			{
+				return null;
+			}
+		}
+
+
+		internal static Task MonitorPlayback()
+		{
+			if(!File.Exists(_nowPlayingFile))
+			{
+				MessageBox.Show($"Now playing file does not exist: \"{_nowPlayingFile}\"", "File Not Found");
+				return Task.CompletedTask;
+			}
+			string lastSong = "";
+			_timer = new Timer(async (x) =>
+			{
+				string nowPlaying = File.ReadAllText(_nowPlayingFile);
+				Debug.WriteLine(nowPlaying);
+				if(nowPlaying != "stopped")
+				{
+					if(nowPlaying.StartsWith("paused:"))
+					{
+						if (lastSong == null)
+							return;
+						lastSong = null;
+						await _client.SetGameAsync(null);
+					}else
+					{
+						string songName = nowPlaying.Remove(0, 9);
+						if (lastSong == songName)
+							return;
+						lastSong = songName;
+						await _client.SetGameAsync(songName);
+					}
+					var u = _client.CurrentUser;
+					Debug.WriteLine((u.Game.HasValue) ? u.Game.Value.Name : "");
+				}
+			}, null, 0, 500);
+			return Task.CompletedTask;
+		}
+	}
+}
